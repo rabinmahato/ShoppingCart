bdcb238a32e62308ef393dd05232496d2ec85044
Rabin Mahato <reach.rabin.mahato@gmail.com>
2018-10-22 12:43:28 +0530

    Upate cart item to db changes pushed

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,15 @@
 import * as actionTypes from './actions';
+import axios from '../items-axios';
 
 const initialState = {
     cartItems : [],
-    totalPrice: 88.00,
+    totalPrice: 0,
     couponDiscount: 7.00,
     editDialogDetails:{},
 };
 
 const reducer = ( state = initialState, action ) => {
+    let totalPrice;
     switch ( action.type ) {
         case actionTypes.UPDATE_EDIT_DIALOG:
             return {
@@ -18,16 +20,24 @@ const reducer = ( state = initialState, action ) => {
             };
 
         case actionTypes.UPDATE_FETCHED_ITEMS:
+            totalPrice = 0
+            for(let index = 0; index < action.items.length; index++){                
+                totalPrice += (action.items[index].quantity * action.items[index].unitPrice);
+            }
             return {
                 ...state,
-                cartItems : action.items
+                cartItems : action.items,
+                totalPrice
             };
 
         case actionTypes.UPDATE_CART_ITEM:
             const aCartItemsCopied = Array.from(state.cartItems);
-            let totalPrice = 0;
+            let itemId;
+            totalPrice = 0;
+
             for(let index = 0; index < aCartItemsCopied.length; index++){                
-                if(aCartItemsCopied[index].style == action.updatedData.cartItem.itemStyle){
+                if(aCartItemsCopied[index].id == action.updatedData.cartItem.itemId){
+                    itemId = action.updatedData.cartItem.itemId;
                     aCartItemsCopied[index].size = action.updatedData.selectedSize;
                     aCartItemsCopied[index].quantity = action.updatedData.selectedQuantity;
                     totalPrice += (action.updatedData.selectedQuantity * aCartItemsCopied[index].unitPrice);
@@ -35,6 +45,29 @@ const reducer = ( state = initialState, action ) => {
                     totalPrice += (aCartItemsCopied[index].quantity * aCartItemsCopied[index].unitPrice);
                 }
             }
+            //Update item in db
+            const oPyaload = {
+                "availableSizes": action.updatedData.cartItem.itemAvailableSizes,
+                "brand": action.updatedData.cartItem.itemBrand,
+                "color": action.updatedData.cartItem.itemColor,
+                "imageURL": action.updatedData.cartItem.imageURL,
+                "key": action.updatedData.cartItem.itemAvailableSizes,
+                "maxQuantity": action.updatedData.cartItem.itemMaxQuantity,
+                "name": action.updatedData.cartItem.itemName,
+                "quantity": action.updatedData.selectedQuantity,
+                "size": action.updatedData.selectedSize,
+                "style": action.updatedData.cartItem.itemStyle,
+                "unitPrice": action.updatedData.cartItem.itemUnitPrice,
+                "key": action.updatedData.cartItem.itemStyle
+            }
+            axios.put(`/shoppingcart-6df6b/${itemId}.json`, oPyaload)
+            .then(response => {
+                console.log('Data updated ', response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
             return {
                 ...state,
                 cartItems: aCartItemsCopied,
