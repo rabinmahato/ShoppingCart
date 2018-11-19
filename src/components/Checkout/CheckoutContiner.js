72f48ad01866142c68fed233a6346bebd79f9b0e
sandip.tiwari@gmail.com <rabmahat@publicisgroupe.net>
2018-11-19 15:42:03 +0530

    empty

--- src/components/Checkout/CheckoutContiner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const checkoutContiner = (props) => (
-    <section>
-        <div className="checkout-container">
-            {props.children}       
-        </div>
-    </section>
-)
-export default checkoutContiner;
--- src/items-axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://shoppingcart-6df6b.firebaseio.com/'
-});
-
-export default instance;
